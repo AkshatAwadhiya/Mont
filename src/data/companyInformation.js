ac3e880881c76f6f83c444aa73514780b346658e
akshat awadhiya <akshatawadhiya@gmail.com>
2025-09-12 00:52:57 +0530

    my first upload

---
+++ src/data/companyInformation.js
@@ -0,0 +1,8 @@
+const companyInformation = [
+    { label: "Founded", value: "2022" },
+    { label: "Employees", value: "200" },
+    { label: "Beta Users", value: "521" },
+    { label: "Raised", value: "Rs10M" },
+];
+
+export default companyInformation;
